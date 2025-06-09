765426e343acf1f3ca22850aae2f749ec544ed0c
KristttoferBruno <kristtoferbruno77@gmail.com>
2025-06-09 15:51:50 -0400

    docs: adiciona comentários explicativos sobre lambdas e streams

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -5,8 +5,10 @@ import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.beans.factory.ListableBeanFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -36,6 +38,7 @@ public class Principal {
 		}
 
 		temporadas.forEach(System.out::println);
+        // :: = o mesmo que temporadas.forEach(t -> System.out.println(t));
 
 //        for(int i = 0; i < dados.totalTemporadas(); i++){
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
@@ -44,6 +47,29 @@ public class Principal {
 //            }
 //        }
 
+        //lambda -> funções que terão um parametro utilizando uma única letra para declarar o parametro
+        // Para cada temporada da lista 'temporadas' (representada por 't'), executa o método 'episodios()'
+        // que retorna a lista de episódios dessa temporada. Em seguida, para cada episódio (representado por 'e'),
+        // imprime no console o título do episódio usando 'System.out.println(e.titulo())'.
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        //Temporada(parametro) -> expressão
+        //o parametro diz, cada uma das temporadas
+
+        // Transforma a lista de nomes em um stream para aplicar operações funcionais:
+// - 'stream()': inicia um fluxo de dados da lista.
+// - 'sorted()': ordena os elementos do stream em ordem natural (alfabética).
+// - 'limit(3)' limita o resultado aos 3 primeiros nomes da lista ordenada.
+// - 'filter(n -> n.startsWith("J"))' filtra os nomes que começam com a letra "J" (case sensitive).
+// - 'map(n -> n.toUpperCase())' transforma os nomes filtrados para letras maiúsculas.
+// - 'forEach(System.out::println)': imprime cada nome ordenado no console.
+
+//        List<String> nomes = Arrays.asList("joao", "Lucas", "maria", "laila");
+//        nomes.stream()
+//                .sorted()
+//                .limit(3)
+//                .filter(n -> n.startsWith("J"))
+//                .map((n -> n.toUpperCase()))
+//                .forEach(System.out::println);
     }
 }
